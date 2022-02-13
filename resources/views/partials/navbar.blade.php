18d25d086f030077668ff38a394b0daacaa3c574
sgungibr <agunggibrhm@gmail.com>
2022-02-13 20:35:56 +0700

    Modul 4

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,7 +17,7 @@
         <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link {{ ( $title === 'Contact') ? 'active' : ''}}" href="/contact">Contact</a>
+        <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : ''}}" href="/contacts">Contacts</a>
       </li>
       
     </ul>
